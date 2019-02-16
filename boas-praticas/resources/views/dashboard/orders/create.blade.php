79067a929504468d06becd643e5ae2661b443025
Bruno Silveira <bruno.silveira@caa.com.br>
2019-02-16 11:27:31 -0400

    required

--- boas-praticas/resources/views/dashboard/orders/create.blade.php
+++ boas-praticas/resources/views/dashboard/orders/create.blade.php
@@ -15,7 +15,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">Status</label>
 
                             <div class="col-md-6">
-                                <select id="status" class="form-control{{ $errors->has('status') ? 'is-invalid' : '' }}" name="status" value="{{ old('status') }}">
+                                <select id="status" class="form-control{{ $errors->has('status') ? 'is-invalid' : '' }}" name="status" value="{{ old('status') }}" required>
                                     <option value="">Selecione o Status</option>
                                     <option value="cancel">Cancelado</option>
                                     <option value="pending">Pendente</option>
@@ -33,7 +33,7 @@
                         <div class="form-group row">
                             <label for="track_code" class="col-md-4 col-form-label text-md-right">Código de Rastreio</label>
                             <div class="col-md-6">
-                                <input id="track_code" type="track_code" class="form-control{{ $errors->has('track_code') ? 'is-invalid' : ''}}" name="track_code" value="{{ old('track_code') }}">
+                                <input id="track_code" type="track_code" class="form-control{{ $errors->has('track_code') ? 'is-invalid' : ''}}" name="track_code" value="{{ old('track_code') }}" required>
 
                                 @if($errors->has('track_code'))
                                     <span class="invalid-feedback" role="alert">
